b31381e7612c15a9a75f829d55297fbc8b08fc29
Joanne Kus <kusjoanne@gmail.com>
2020-02-29 22:09:59 -0600

    completed simon game

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -1,23 +1,65 @@
 var sequence = [];
 var userSequence = [];
 var colors = ['red','green','blue','yellow'];
+var level = 0;
 
 $(document).keypress(function(){
   generateSequence();
-})
+});
 
 function generateSequence(){
   let newNumber= Math.floor(Math.random()*4);
-  sequence.push(colors[newNumber]);
-  $("#"+colors[newNumber]).fadeOut(100).fadeIn(100);
-  console.log(sequence);
-  getUserInput();
+  let newColor= colors[newNumber];
+  sequence.push(newColor);
+  $("#"+newColor).fadeOut(100).fadeIn(100);
+  playSound(newColor);
+  console.log("sequence " + sequence);
+  level++;
+  $("h1").text("Level " + level);
+  userSequence = [];
 }
 
-function getUserInput(sequenceLength){
-  $(".btn").click(function(){
-    $("#"+this.id).fadeOut(100).fadeIn(100);
-    userSequence.push(this.id);
+$(".btn").click(function(){
+  var userColor = $(this).attr("id");
+  userSequence.push(userColor);
+  animatePress(userColor);
+  playSound(userColor);
+  console.log("user sequence " + userSequence);
+  checkAnswer(userSequence.length-1);
+});
+
+function checkAnswer(lastItem){
+  if(userSequence[lastItem] == sequence[lastItem]){
+    if(userSequence.length === sequence.length){
+      setTimeout(function () {
+        generateSequence();
+      }, 1000);
+    }
+  }
+  else{
+    $("h1").text("Game Over, Press Any Key to Restart ");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    startOver();
+  }
+}
+
+function playSound(color){
+  var audio = new Audio("sounds/"+color+".mp3");
+  audio.play();
+}
+
+function animatePress(color){
+  $("#"+color).addClass("pressed");
+  setTimeout(function(){
+    $("#"+color).removeClass("pressed");
   });
- console.log(userSequence);
+}
+
+function startOver(){
+  level = 0;
+  sequence = [];
+  userSequence = [];
 }
